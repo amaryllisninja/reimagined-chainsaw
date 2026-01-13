72d74c84dd482db830f9e68fc5ad6049f6a621c4
Amaryllis Graybill <amaryllisgraybill@gmail.com>
2026-01-12 19:48:30 -0800

    Episode 7 commit

--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.http import Http404
-from django.views.generic import DetailView, ListView, CreateView
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404, HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic import DetailView, ListView, CreateView, UpdateView
+from django.views.generic.edit import DeleteView
 
 from .forms import NotesForm
 from .models import Notes
@@ -19,3 +21,18 @@ class NotesCreateView(CreateView):
     success_url = '/smart/notes'
     form_class = NotesForm
 
+class NotesUpdateView(UpdateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm
+
+class NotesDeleteView(DeleteView):
+    model = Notes
+    success_url = '/smart/notes'
+    template_name = "notes/notes_delete.html"
+
+def add_like_view(request, pk):
+    note = get_object_or_404(Notes, pk=pk)
+    note.likes += 1
+    note.save()
+    return HttpResponseRedirect(reverse("notes.detail", args=(pk,)))
